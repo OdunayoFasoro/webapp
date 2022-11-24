b35821533abd0e3f69b6448fda8acb6b5418fe63
OdunayoFasoro <oofasoro@gmail.com>
2022-11-24 04:30:07 +0000

    praxis

---
+++ app2.sql
@@ -0,0 +1,10 @@
+#this is code for daily journal
+#please do not hard code 
+# use variables 
+#author : Odunayomimare 
+echo "Today is a dood day to pen down your jpurney through life"
+echo "Hello!, my name is ThePath and i am your personal journal what is yoiour name?"
+# name= $name 
+print name 
+read name 
+echo "Hi $name, i am excited to walk this journey with you"
